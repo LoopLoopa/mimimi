1034b7a05f25cbf0e65a045b0555f92a4305e183
LoopLoopa <43594263+LoopLoopa@users.noreply.github.com>
2025-01-29 17:03:14 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import lovesvg from "./assets/me n mimi.jpg";
+import lovesvg2 from "./assets/love factory.jpg";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "i think u pressed the wrong button lol",
+      "ummmmmmm click the button to the left hahahahahah",
+      "water u doing.",
+      "WHY",
+      "every press is another puppy dead (me)",
+      "STOP IT",
+      "PLS",
+      "U DONT HAVE TO KEEP CLICKING THIS",
+      "red means not good btw haha just thought u should know",
+      "have a HEART HOBEY PLEASE",
+      "IM GONNA THROW UP",
+      "THE GREEN BUTTON HOBEY PLEASE",
+      ":c",
+      "WAAHHHHHHHHHHHHHHHHHHH",
+      "u dont love me ;(",
+      "u actually hate me",
+      "ykw just know that no matter how much u hate me i still love u",
+      "*floats into space and pops*",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="text-4xl md:text-6xl font-bold my-4">
+            WOOHOO!!!!!
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            src={lovesvg}
+            className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
+          />
+          <img
+            src={lovesvg2}
+            className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
+          />
+          <img
+            className="h-[230px] rounded-lg shadow-lg"
+            src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="text-4xl md:text-6xl my-4 text-center">
+            Will you be my Valentine?
+          </h1>
+          <div className="flex flex-wrap justify-center gap-2 items-center">
+            <button
+              className={`bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-4`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <a
+      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
+      href="https://github.com/Xeven777/valentine"
+      target="__blank"
+    >
+      Made with{" "}
+      <span role="img" aria-label="heart">
+        ❤️
+      </span>
+    </a>
+  );
+};
